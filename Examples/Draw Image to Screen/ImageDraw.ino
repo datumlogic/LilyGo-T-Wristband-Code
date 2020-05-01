723ac012cd2015078f45fb480222fc63b6b46eb8
Sam Warr <64652414+sammothxc@users.noreply.github.com>
2020-05-01 16:39:14 -0500

    Update ImageDraw.ino

--- Examples/Draw Image to Screen/ImageDraw.ino
+++ Examples/Draw Image to Screen/ImageDraw.ino
@@ -13,7 +13,16 @@ TFT_eSPI tft = TFT_eSPI();                      // Invoke library, pins defined 
 void setup(void)                                // SETUP
 {
   tft.init();                                   // Start the display
-  tft.setRotation(1);                           // This sets the screen orientation so that the touch button is to the right of the display
+  tft.setRotation(3);                           // This sets the screen orientation so that the touch button is to the left of the display
+  
+  //  0 = touch button is under the display     ***IMAGE APPEARS GARBLED THOUGH***
+  
+  //  1 = touch button on left side for right-handed people (assuming you wear your watch on the left side)
+  
+  //  2 = touch button is over the display      ***IMAGE APPEARS GARBLED THOUGH***
+  
+  //  3 = touch button on left side for left-handed people (assuming you wear your watch on the right side)
+  
   tft.setSwapBytes(true);                       // Needed for reading the .h file correctly
   tft.pushImage(0, 0,  160, 80, SOMETHING);     // This is the line that writes the image-- make sure you change SOMETHING to the name of your file
   pinMode(LED_PIN, OUTPUT);                     // Setup LED pin
