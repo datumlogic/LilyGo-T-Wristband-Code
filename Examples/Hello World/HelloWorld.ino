70f0f0db7d64e7f6164ccc401b47ba8923fa28ca
Sam Warr <64652414+sammothxc@users.noreply.github.com>
2020-05-01 16:49:43 -0500

    Update HelloWorld.ino

--- Examples/Hello World/HelloWorld.ino
+++ Examples/Hello World/HelloWorld.ino
@@ -14,8 +14,6 @@ void setup(void)                                // SETUP
   tft.init();                                   // Start the display
   tft.setSwapBytes(true);                       // Needed for reading .h files correctly
   tft.setTextColor(TFT_BLUE, TFT_BLACK);        // Sets Font-- this does not draw the background color for all of the screen
-  tft.fillScreen(TFT_BLACK);                    // Gotta clear the screen or else
-  tft.setCursor (80, 40);                       // Move our "pen" to the center of the screen
   pinMode(LED_PIN, OUTPUT);                     // Setup LED pin
 }
 
@@ -25,9 +23,11 @@ void loop()                                     // LOOP
   {
     tft.setRotation(i);                         // This sets the screen orientation to i
     tft.fillScreen(TFT_BLACK);                  // Gotta clear the screen or else
+    tft.setCursor (80, 40);                       // Move our "pen" to the center of the screen
     tft.printf("HelloWorld");                   // This uses the standard ADAFruit small font-- very basic API, recommend using drawString()
     digitalWrite(LED_PIN, HIGH);                // Turn on LED...
     delay(500);                                 // ...for .5 sec
     digitalWrite(LED_PIN, LOW);                 // Turn it off
+    delay(500);
   }
 }
