042d5bc9f7699f0424ad3cb4ef622325e70218f7
Sam Warr <64652414+sammothxc@users.noreply.github.com>
2020-05-01 16:42:38 -0500

    Update HelloWorld.ino

--- Examples/Hello World/HelloWorld.ino
+++ Examples/Hello World/HelloWorld.ino
@@ -1,1 +1,31 @@
+#include <TFT_eSPI.h>                           // Graphics and font library for ST7735 driver chip
+#include <SPI.h>                                // Communication to the screen happens over SPI
 
+#ifndef ST7735_SLPIN
+#define ST7735_SLPIN 0x10                       // Setup some commands for our specific screen
+#endif
+
+#define LED_PIN 4                               // LED is on Pin 4
+
+TFT_eSPI tft = TFT_eSPI();                      // Invoke library, pins defined in User_Setup.h
+
+void setup(void)                                // SETUP
+{
+  tft.init();                                   // Start the display
+  tft.setSwapBytes(true);                       // Needed for reading .h files correctly
+  tft.setTextColor(TFT_BLUE, TFT_BLACK);        // Sets Font-- this does not draw the background color for all of the screen
+  tft.setCursor (80, 40);                        // Move our "pen" to the center of the screen
+  pinMode(LED_PIN, OUTPUT);                     // Setup LED pin
+}
+
+void loop()                                     // LOOP
+{
+  for(int i = 0; i > -1; i++)                   // Lets count i up
+  {
+    tft.setRotation(i);                         // This sets the screen orientation to i
+    tft.printf("HelloWorld");                      // This uses the standard ADAFruit small font
+    digitalWrite(LED_PIN, HIGH);                // Turn on LED...
+    delay(500);                                 // ...for .5 sec
+    digitalWrite(LED_PIN, LOW);                 // Turn it off
+  }
+}
